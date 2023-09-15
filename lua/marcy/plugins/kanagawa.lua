948492135125f15be1ba562154a07aa331558cb3
Marcus Holdinghausen <derrr_dude@gmx.de>
2023-09-15 12:21:10 +0200

    add even more options

--- lua/marcy/plugins/kanagawa.lua
+++ lua/marcy/plugins/kanagawa.lua
@@ -1,6 +1,8 @@
 function LineNumberColors()
-    vim.api.nvim_set_hl(0, 'LineNrAbove', { fg = '#ADD8E6', bold = true })
-    vim.api.nvim_set_hl(0, 'LineNrBelow', { fg = '#ADD8E6', bold = true })
+    -- vim.api.nvim_set_hl(0, 'LineNrAbove', { fg = '#ADD8E6', bold = true })
+    -- vim.api.nvim_set_hl(0, 'LineNrBelow', { fg = '#ADD8E6', bold = true })
+    vim.api.nvim_set_hl(0, 'LineNrAbove', { fg = '#627d9a', bold = true })
+    vim.api.nvim_set_hl(0, 'LineNrBelow', { fg = '#627d9a', bold = true })
     vim.api.nvim_set_hl(0, 'Visual', { bg = '#627d9a', bold = true })
     -- vim.api.nvim_set_hl(0, 'Visual', { bg = 'blue', bold = true })
 end
