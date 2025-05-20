b942970e18b1353f45848abc18f2a5a64880abc5
Marcus Holdinghausen <derrr_dude@gmx.de>
2025-05-20 15:55:42 +0200

    chore: try blink and sources

--- lua/marcy/plugins/database.lua
+++ lua/marcy/plugins/database.lua
@@ -1,23 +1,41 @@
 return {
-	"kristijanhusak/vim-dadbod-ui",
-	commit = "0f51d8de368c8c6220973e8acd156d17da746f4c",
-	dependencies = {
-		{ "tpope/vim-dadbod", lazy = true },
-		{
-			"kristijanhusak/vim-dadbod-completion",
-			ft = { "sql", "mysql", "plsql" },
-			lazy = true,
+	{
+		"kristijanhusak/vim-dadbod-ui",
+		commit = "0f51d8de368c8c6220973e8acd156d17da746f4c",
+		dependencies = {
+			{ "tpope/vim-dadbod", lazy = true },
+			{
+				"kristijanhusak/vim-dadbod-completion",
+				ft = { "sql", "mysql", "plsql" },
+				lazy = true,
+			},
+			-- 'kristijanhusak/vim-dadbod-completion',
 		},
-		-- 'kristijanhusak/vim-dadbod-completion',
+		cmd = {
+			"DBUI",
+			"DBUIToggle",
+			"DBUIAddConnection",
+			"DBUIFindBuffer",
+		},
+		config = function()
+			-- Your DBUI configuration
+			vim.g.db_ui_use_nerd_fonts = 1
+		end,
+
 	},
-	cmd = {
-		"DBUI",
-		"DBUIToggle",
-		"DBUIAddConnection",
-		"DBUIFindBuffer",
-	},
-	config = function()
-		-- Your DBUI configuration
-		vim.g.db_ui_use_nerd_fonts = 1
-	end,
+	{ -- optional saghen/blink.cmp completion source
+		'saghen/blink.cmp',
+		opts = {
+			sources = {
+				default = { "lsp", "path", "snippets", "buffer" },
+				per_filetype = {
+					sql = { 'snippets', 'dadbod', 'buffer' },
+				},
+				-- add vim-dadbod-completion to your completion providers
+				providers = {
+					dadbod = { name = "Dadbod", module = "vim_dadbod_completion.blink" },
+				},
+			},
+		},
+	}
 }
