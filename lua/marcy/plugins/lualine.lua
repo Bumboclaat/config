f967697e053694be209fa6b1f30c233e27dacc04
Marcus Holdinghausen <derrr_dude@gmx.de>
2025-06-15 12:28:05 +0200

    chore: add branch diff to lua line

--- lua/marcy/plugins/lualine.lua
+++ lua/marcy/plugins/lualine.lua
@@ -1,28 +1,33 @@
 return {
-  'nvim-lualine/lualine.nvim',
+  "nvim-lualine/lualine.nvim",
   -- Set lualine as statusline
   -- See `:help lualine.txt`
   config = function()
-    require('lualine').setup {
+    require("lualine").setup({
       options = {
         icons_enabled = true,
-        theme = 'onedark',
-        component_separators = '|',
-        section_separators = '',
+        theme = "onedark",
+        component_separators = "|",
+        section_separators = "",
       },
       sections = {
+        lualine_b = {
+          "branch",
+          "diff",
+        },
         lualine_c = {
           {
-            'filename',
+            "filename",
             file_status = true, -- displays file status (readonly status, modified status)
-            path = 1            -- 0 = just filename, 1 = relative path, 2 = absolute path
-          }
+            path = 1,     -- 0 = just filename, 1 = relative path, 2 = absolute path
+          },
         },
         lualine_x = {
-          'encoding', 'filetype'
+          "encoding",
+          "filetype",
         },
-        lualine_y = {}
-      }
-    }
-  end
+        lualine_y = {},
+      },
+    })
+  end,
 }
