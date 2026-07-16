7606f5a71dbe39a6aefd71cc058da822cca9209a
Marcus Holdinghausen <marcus.holdinghausen@ororatech.com>
2026-07-16 11:44:11 +0200

    chore: add db qicktoggle

--- lua/marcy/plugins/alpha.lua
+++ lua/marcy/plugins/alpha.lua
@@ -20,6 +20,7 @@ return {
             dashboard.button("e", "  > New file", ":ene <BAR> startinsert <CR>"),
             dashboard.button("f", "󰈞  > Find file", ":Telescope find_files<CR>"),
             dashboard.button("r", "  > Recent", ":Telescope oldfiles<CR>"),
+            dashboard.button("d", "  > Database", ":DBUIToggle<CR>:wincmd p<CR>:close<CR>"),
             dashboard.button("s", "  > Settings", ":e $MYVIMRC | :cd %:p:h | split . | wincmd k | pwd<CR>"),
             dashboard.button("q", "󰅚  > Quit NVIM", ":qa<CR>"),
         }
